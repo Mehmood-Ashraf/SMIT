5e98874d3706051ed96aa05ecee3f0918b12dd31
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2024-08-28 01:58:47 +0500

    Adding Chapter 14-16 Assignment

---
+++ Javascript/Book Assignments/Chapter 14-16 Assignment/app.js
@@ -0,0 +1,18 @@
+// Chapter 14-16 Assignment
+//Arrays
+
+//Empty Array
+var studentNames = [];
+
+
+//String Array
+var fruitNames = ["Mango", "Banana", "Strawberry", "Apple"];
+
+// Numbers Array
+var numbers = [1, 2, 3, 4, 5];
+
+//Boolean Array
+var isStudetns = [true, false];
+
+//Mixed Array
+var mixedArray = ["Mehmood", 4, 5, true, false];
