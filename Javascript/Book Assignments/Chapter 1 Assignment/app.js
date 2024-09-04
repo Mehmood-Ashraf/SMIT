2678b900fa46293c9c978974b8c4f1be1d44fb39
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2024-09-05 01:24:01 +0500

    add commits

--- Javascript/Book Assignments/Chapter 1 Assignment/app.js
+++ Javascript/Book Assignments/Chapter 1 Assignment/app.js
@@ -2,10 +2,15 @@
 //Chapter # 1
 //Alerts
 
+
+/* Question 1 */
+
 alert("Welcome to our Website");
 
+/* Question 2*/
 alert("Error! Please enter a valid password");
 
+/* Question 3 */
 alert("Welcome to JS Land.... \nHappy Coding!");
 
 alert("Welcome to JS Land....");
